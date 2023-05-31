956c618318f3deee11d49658654f8622573a2a4a
Oleksandr <fedorut20@gmail.com>
2023-06-01 00:44:49 +0300

    task done #3

--- cpp_array.cpp
+++ cpp_array.cpp
@@ -4,6 +4,22 @@
 #include <iostream>
 using namespace std;
 
-void main(){}
+int main()
+{
+    int sum_side = 0;
+    const int size = 5;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Side " << i + 1 << ": ";
+        cin >> arr[i];
+        sum_side += arr[i];
+    }
+
+    cout << "Total: " << sum_side << endl;
+
+    return 0;
+}
+
 
 // In the future, to open this project again, go to File > Open > Project and select the .sln file
