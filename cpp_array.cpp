acd62f754ca619fa57b56f76a61b36280b8e881f
Oleksandr <fedorut20@gmail.com>
2023-06-01 00:15:14 +0300

    task done

--- cpp_array.cpp
+++ cpp_array.cpp
@@ -1,9 +1,25 @@
 // cpp_array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 
 
 #include <iostream>
 using namespace std;
 
-void main(){}
+int main()
+{
+    int sum = 0;
+    const int size = 6;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Profit for month " << i + 1 << ": ";
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Total profit: " << sum << endl;
+
+    return 0;
+}
 
 // In the future, to open this project again, go to File > Open > Project and select the .sln file
