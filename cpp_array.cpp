1c9ceb8975ccd7af2cfd721dbdac6e6bc93fd2c8
Oleksandr <fedorut20@gmail.com>
2023-06-01 00:07:02 +0300

    Add project files.

---
+++ cpp_array.cpp
@@ -0,0 +1,9 @@
+// cpp_array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void main(){}
+
+// In the future, to open this project again, go to File > Open > Project and select the .sln file
