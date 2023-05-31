c809dffdef808056f347d0936d7f113d69358b18
Oleksandr <fedorut20@gmail.com>
2023-06-01 00:38:44 +0300

    task done

--- cpp_array.cpp
+++ cpp_array.cpp
@@ -4,6 +4,17 @@
 #include <iostream>
 using namespace std;
 
-void main(){}
+int main()
+{
+    const int SIZE = 10;
+    int arr[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    for (int i = SIZE - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
 
 // In the future, to open this project again, go to File > Open > Project and select the .sln file
