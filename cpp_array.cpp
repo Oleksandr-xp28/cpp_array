e13e4541eb9b8600a7e5c64dc42a41c45aa38ca9
Oleksandr <fedorut20@gmail.com>
2023-06-01 00:56:54 +0300

    task done

--- cpp_array.cpp
+++ cpp_array.cpp
@@ -4,6 +4,24 @@
 #include <iostream>
 using namespace std;
 
-void main(){}
+int main()
+{
+    int max;
+    const int size = 12;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Profit for month " << i + 1 << ": ";
+        cin >> arr[i];
+        if (i == 0 || arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    cout << "Max: " << max << endl;
+
+    return 0;
+}
 
 // In the future, to open this project again, go to File > Open > Project and select the .sln file
