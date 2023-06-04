ee1990a0bbc29954d0720b301fc23e6895579662
Oleksandr <fedorut20@gmail.com>
2023-06-05 02:17:41 +0300

    done

--- cpp_array.cpp
+++ cpp_array.cpp
@@ -7,15 +7,21 @@ using namespace std;
 int main() {
 
     int M;
+
     cout << "Enter the size of the array: ";
     cin >> M;
+
     double* arr = new double[M]; // Покажчик
+
     cout << "Enter the elements of the array: ";
+
     for (int i = 0; i < M; i++) cin >> arr[i];
 
     double sumNeg = 0;
     double prodEven = 1;
+
     int firstNeg = -1, lastNeg = -1;
+
     for (int i = 0; i < M; i++) {
         if (arr[i] < 0) {
             sumNeg += arr[i];
@@ -26,6 +32,7 @@ int main() {
     }
 
     double sumBetweenNeg = 0;
+
     if (firstNeg != -1 && lastNeg != -1)
         for (int i = firstNeg + 1; i < lastNeg; i++)
             sumBetweenNeg += arr[i];
